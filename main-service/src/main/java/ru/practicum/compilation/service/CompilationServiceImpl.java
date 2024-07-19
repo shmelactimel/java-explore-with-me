7bdc10707a21322ec34495589662488a32f8ef7d
shmelactimel <2002erra@gmail.com>
2024-07-19 15:45:21 -0400

    Step 2: refactor in CompilationDto and CompilationServiceImpl fix

--- main-service/src/main/java/ru/practicum/compilation/service/CompilationServiceImpl.java
+++ main-service/src/main/java/ru/practicum/compilation/service/CompilationServiceImpl.java
@@ -1,7 +1,6 @@
 package ru.practicum.compilation.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
