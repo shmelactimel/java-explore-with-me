305ddbc34ef6128ae0435b224de0e68ac064a1d0
shmelactimel <2002erra@gmail.com>
2024-07-19 15:25:36 -0400

    Step 2: refactor in CategoryServiceImpl and MainServiceExceptionHandler fix

--- main-service/src/main/java/ru/practicum/exception/MainServiceExceptionHandler.java
+++ main-service/src/main/java/ru/practicum/exception/MainServiceExceptionHandler.java
@@ -2,6 +2,7 @@ package ru.practicum.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
