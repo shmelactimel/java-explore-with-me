e92c43b446bd2e7fcc5f17408cb41dd04764dc79
shmelactimel <2002erra@gmail.com>
2024-09-01 01:05:40 -0400

    Iteration 1.2.2.18

--- main-service/src/main/java/ru/practicum/category/dto/CategoryDto.java
+++ main-service/src/main/java/ru/practicum/category/dto/CategoryDto.java
@@ -7,7 +7,7 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode(callSuper=true)
+@EqualsAndHashCode(callSuper = true)
 public class CategoryDto extends NewCategoryDto {
     private Long id;
 }
