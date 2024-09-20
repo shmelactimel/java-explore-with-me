02c74032ab5b39105442719240040a4f623e48cd
shmelactimel <2002erra@gmail.com>
2024-09-19 22:10:44 -0400

    Iteration 3 fix v.2 Refactor: created dto EventCommentCountDto for transfer data about how many feedbacks per events; added at the interface a new method? which return list of objects EventCommentCountDto; Implemented getEventCommentCounts() in service:

--- main-service/src/main/java/ru/practicum/comment/controller/FeedbackController.java
+++ main-service/src/main/java/ru/practicum/comment/controller/FeedbackController.java
@@ -22,6 +22,7 @@ import java.util.List;
 public class FeedbackController {
 
     private final FeedbackService feedbackService;
+    private final EventService eventService;
 
     @PostMapping("/events/{eventId}/feedbacks")
     @ResponseStatus(HttpStatus.CREATED)
